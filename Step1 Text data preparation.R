7073d00ebac0ab9c7afdd2011d072b4d81b35d17
DrKevin22 <kevin.na74@gmail.com>
2017-11-03 07:54:17 +0900

    코드 일부 수정 + 주석 추가

--- Step1 Text data preparation.R
+++ Step1 Text data preparation.R
@@ -1,13 +1,14 @@
 
+
 #--------------------------------------------------------------------------
 # 1. 크롤러로 수집한 텍스트 데이터 정리
 #--------------------------------------------------------------------------
 
 # 작업경로 설정
-wdir <- "~/Data"
+wdir <- "~/Desktop/Crawler/Data"
 
 # 텍스트 데이터 목록 확인
-(fileList <- list.files(path=wdir))
+(fileList <- list.files(path = wdir))
 
 # 작업일자 지정
 (today <- gsub("-", "", Sys.Date()))
@@ -19,25 +20,22 @@ month <- "201701"
 (docName <- paste(type, query, month, sep = "_"))
 
 # 위에서 지정한 파일만 가지고 오기
-(fileList <- grep(docName, fileList, value=T))
+(fileList <- grep(docName, fileList, value = T))
 
 # 엑셀파일 불러오기
 text <- data.frame()
 
 library(readxl)
 for (i in 1:length(fileList)) {
-  blog <- read_excel(paste(wdir, fileList[i], sep="/"))
+  blog <- read_excel(paste(wdir, fileList[i], sep = "/"))
   blog <- as.data.frame(blog)
   text <- rbind(text, blog)
 }
 
 rm(blog)
 
-# 블로그 요약과 본문 합치기 (블로그)
-# text$content <- paste(text$summary, text$fulltext)
-# text$content <- gsub("NA", "", text$content)
-
-# 컬럼명 변경하기 (형태소 분석기에 특정된 컬럼명임!)
+# 컬럼명 변경하기
+# 앞으로 사용하려는 형태소 분석기에 특정된 컬럼명인 "content"으로 변경!
 colnames(text)[7] <- "content"
 
 # 컬럼 순서 변경
@@ -45,20 +43,20 @@ colnames(text)[7] <- "content"
 
 # 텍스트마이닝 데이터 만들기
 # 20170101 이후 포스팅만
-tmdf <- text[text$postdate >= "2017.01.01", ]
+tmdf <- text[text$postdate >= "2017.01.01",]
 tmdf$id <- seq(1, nrow(tmdf))
 
 # 링크로 중복 제거
 library(dplyr)
-tmdf <- distinct(tmdf, link, .keep_all=T)
+tmdf <- distinct(tmdf, link, .keep_all = T)
 
 # 필요한 컬럼만 남기기
 tmdf <- tmdf[, c("id", "keyword", "postdate", "bloger", "content")]
 
-# content 컬럼이 NA인 건 제거 
-tmdf <- tmdf[is.na(tmdf$content)==F,]
+# content 컬럼이 NA인 건 제거
+tmdf <- tmdf[is.na(tmdf$content) == F, ]
 
 
 # Rdata로 저장하기
 save.image(paste0(docName, ".Rdata"))
-list.files(pattern="Rdata")
+list.files(pattern = "Rdata")
--- Step5 Create corpus and DTM.R
+++ Step5 Create corpus and DTM.R
@@ -1,23 +1,24 @@
 
+
 #--------------------------------------------------------------------------
 # 5. 말뭉치(corpus) 생성
 #--------------------------------------------------------------------------
 
-# Rdata 확인 
-list.files(pattern="Rdata")
+# Rdata 확인
+list.files(pattern = "Rdata")
 
 # Rdata 불러오기
 load("Blog_지름신_201701.Rdata")
 
 
-# 말뭉치를 생성하기 전에 텍스트를 벡터 소스로 변경 
+# 말뭉치를 생성하기 전에 텍스트를 벡터 소스로 변경
 # 벡터 소스는 벡터의 개별 원소를 각각의 문서로 인식
 library(tm)
 (corpus <- VCorpus(VectorSource(parsed)))
 
-# 결과 객체(corpus)는 content와 meta를 요소로 갖는 리스트 
+# 결과 객체(corpus)는 content와 meta를 요소로 갖는 리스트
 # content에는 한글 텍스트, meta에는 데이터 속성이 할당됨
-# 첫 번째 결과 확인 
+# 첫 번째 결과 확인
 # str(corpus[[1]])
 corpus[[1]]$content
 corpus[[1]]$meta
@@ -67,10 +68,18 @@ class(corpus[[1]])
 
 # Document Term Matrix 생성 (Term frequency)
 # 단어 Length는 2로 세팅
-(dtm <- DocumentTermMatrix(corpus, control=list(wordLengths=c(2,Inf))))
+# 생성된 dtm을 출력해보면 여러 정보를 확인할 수 있음
+# 이 DTM은 4555개 문서, 38658개 단어로 구성된 행렬 (38658 차원임!)
+# sparsity는 전체 행렬 중 0이 차지하는 비중을 의미
+# weighting은 행렬을 구성하는 각각의 값(value)을 계산한 방식을 나타냄
+(dtm <-
+    DocumentTermMatrix(corpus, control = list(wordLengths = c(2, Inf))))
+
+# 단어(=컬럼명) 양옆 스페이스 제거
+(colnames(dtm) <- trimws(colnames(dtm)))
 
 # 문서 이름(row name) 지정
-dtm$dimnames$Docs <- paste(docName, 1:length(parsed), sep="_")
+dtm$dimnames$Docs <- paste(docName, 1:length(parsed), sep = "_")
 dtm$dimnames$Docs[1:10]
 dtm$dimnames$Terms[1:10]
 
@@ -80,11 +89,12 @@ dtm$dimnames$Terms[1:10]
 
 # 작업의 편의를 위해 대부분이 0인 sparse Terms 일부 삭제
 # 설정하는 값의 크기가 작을수록 term의 개수 감소
+# 각각의 열(단어) 기준으로 sparsity가 0.99를 넘는 열을 삭제
+# 그 결과 문서의 개수는 4555개로 변함 없으나, 단어의 개수는 2389로 크게 감소
 (dtm <- removeSparseTerms(dtm, as.numeric(0.99)))
 
-
-# DTM을 데이터 프레임 형식으로 저장 
-# 나중에 분류모형을 위해 미리 작업!!
+# DTM을 데이터 프레임 형식으로 저장
+# 나중에 토픽 클러스터링 또는 분류모형을 위해 미리 작업!!
 dtmDf <- as.data.frame(as.matrix(dtm))
 dim(dtmDf)
 head(dtmDf)
@@ -92,3 +102,4 @@ head(dtmDf)
 
 # Rdata로 저장하기
 save.image(paste0(docName, ".Rdata"))
+list.files(pattern = "Rdata")
